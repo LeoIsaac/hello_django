ced0b6119c6745bf242c0318bcd9a02dedefc277
Isaac <r.sato7690@me.com>
2016-12-28 01:33:20 +0900

    Modify routing

--- polls/views.py
+++ polls/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 from django.http import Http404
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.template import loader
 from .models import Question
 
@@ -13,11 +13,8 @@ def index(request):
     return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-        context = { 'question': question }
-    except Question.DoesNotExist:
-        raise Http404("その質問は存在しません。")
+    question = get_object_or_404(Question, pk=question_id)
+    context = { 'question': question }
     return render(request, 'polls/detail.html', context)
 
 def results(request, question_id):
