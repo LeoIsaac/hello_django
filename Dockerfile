3b32a35a110cccacc5de194daa4d412f04e3e7c7
Isaac <r.sato7690@me.com>
2016-12-27 17:52:31 +0900

    Add db
    ただしDjango側から書き込もうとすると、よく分からんエラーが発生する

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,7 @@
 FROM python:3.5.2-alpine
+
+RUN apk update && \
+    apk add --no-cache gcc postgresql-dev musl-dev postgresql-client
+
 ADD requirements.txt /
 RUN pip install -r requirements.txt
--- hello_django/settings.py
+++ hello_django/settings.py
@@ -73,10 +73,21 @@ WSGI_APPLICATION = 'hello_django.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'hello',
+        'USER': 'hello',
+        'PASSWORD': 'hello',
+        'HOST': 'db',
+        'PORT': 5432
     }
 }
 
