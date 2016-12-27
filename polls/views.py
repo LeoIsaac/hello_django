0795d79abd1c8446a57187934fcbb4f6ed8d58bf
Isaac <r.sato7690@me.com>
2016-12-28 02:40:24 +0900

    Modify vote

--- polls/views.py
+++ polls/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.http import HttpResponse
-from django.http import Http404
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.template import loader
-from .models import Question
+from django.urls import reverse
+from .models import Question, Choice
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
@@ -18,7 +18,21 @@ def detail(request, question_id):
     return render(request, 'polls/detail.html', context)
 
 def results(request, question_id):
-    return HttpResponse("あなたが探しているのは%sの結果ですね。" % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    context = { 'question': question }
+    return render(request, 'polls/results.html', context)
 
 def vote(request, question_id):
-    return HttpResponse("あなたは質問%sに投票しました。" % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    context = {
+        'question': question,
+        'error_message': "あなたは何も選択していません。"
+    }
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', context)
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
