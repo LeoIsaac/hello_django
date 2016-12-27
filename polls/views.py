c0dcdd384287db3386c26354fa82414a3ae64681
Isaac <r.sato7690@me.com>
2016-12-28 00:55:17 +0900

    Add polls routing

--- polls/views.py
+++ polls/views.py
@@ -2,6 +2,18 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from .models import Question
 
 def index(request):
-    return HttpResponse("Hello, world! ここはpollsのトップページです。")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = ', '.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
+
+def detail(request, question_id):
+    return HttpResponse("あなたが探しているのは%sですね。" % question_id)
+
+def results(request, question_id):
+    return HttpResponse("あなたが探しているのは%sの結果ですね。" % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("あなたは質問%sに投票しました。" % question_id)
