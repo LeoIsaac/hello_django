e45b1de6c471c89274b848e1d8b5a293f0e19b2d
Isaac <r.sato7690@me.com>
2016-12-28 01:21:18 +0900

    Add polls detail 404

--- polls/views.py
+++ polls/views.py
@@ -2,15 +2,23 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from django.http import Http404
+from django.shortcuts import render
+from django.template import loader
 from .models import Question
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    output = ', '.join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
+    context = { 'latest_question_list': latest_question_list }
+    return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-    return HttpResponse("あなたが探しているのは%sですね。" % question_id)
+    try:
+        question = Question.objects.get(pk=question_id)
+        context = { 'question': question }
+    except Question.DoesNotExist:
+        raise Http404("その質問は存在しません。")
+    return render(request, 'polls/detail.html', context)
 
 def results(request, question_id):
     return HttpResponse("あなたが探しているのは%sの結果ですね。" % question_id)
